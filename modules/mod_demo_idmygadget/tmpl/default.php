5b275187229bda6e61e89dc92f6d3496a9f6fd0b
Tom W Hartung <tomwhartung@gmail.com>
2015-08-28 15:31:23 -0600

    Fixed that little issue.

--- modules/mod_demo_idmygadget/tmpl/default.php
+++ modules/mod_demo_idmygadget/tmpl/default.php
@@ -13,13 +13,17 @@ defined('_JEXEC') or die;
 
 <div class="custom<?php echo $moduleclass_sfx ?>" <?php if ($params->get('backgroundimage')) : ?> style="background-image:url(<?php echo $params->get('backgroundimage');?>)"<?php endif;?> >
 	<?php echo $module->content;?>
-	<?php $uri = & JFactory::getURI(); ?>
+	<?php
+		$uriRaw = & JFactory::getURI();
+		$strposQuestionMark = strpos( $uri, '?' );
+		$uriClean = substr( $uriRaw, 0, $strposQuestionMark );
+	?>
 	<ul>
-		<li><a href="<?php print $uri . '?gadgetType=phone'; ?>" title="Emulate Phone">
+		<li><a href="<?php print $uriClean . '?gadgetType=phone'; ?>" title="Emulate Phone">
 			Emulate Phone</a></li>
-		<li><a href="<?php print $uri . '?gadgetType=tablet'; ?>" title="Emulate Tablet">
+		<li><a href="<?php print $uriClean . '?gadgetType=tablet'; ?>" title="Emulate Tablet">
 			Emulate Tablet</a></li>
-		<li><a href="<?php print $uri . '?gadgetType=desktop'; ?>" title="Emulate Desktop">
+		<li><a href="<?php print $uriClean . '?gadgetType=desktop'; ?>" title="Emulate Desktop">
 			Emulate Desktop</a></li>
 	</ul>
 </div>
