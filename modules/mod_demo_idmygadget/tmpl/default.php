64b651a3785bdf565d45def0bade4c49450645d1
Tom W Hartung <tomwhartung@gmail.com>
2015-08-30 07:47:59 -0600

    Updated to create the anchor tag html programmatically, so it is easy to see which view is being rendered (because that option is text, not a link).

--- modules/mod_demo_idmygadget/tmpl/default.php
+++ modules/mod_demo_idmygadget/tmpl/default.php
@@ -14,6 +14,9 @@ defined('_JEXEC') or die;
 <div class="custom<?php echo $moduleclass_sfx ?>" <?php if ($params->get('backgroundimage')) : ?> style="background-image:url(<?php echo $params->get('backgroundimage');?>)"<?php endif;?> >
 	<?php echo $module->content;?>
 	<?php
+		//
+		// Get the uri of the current page and remove anything after '?'
+		//
 		$uriRaw = & JFactory::getURI();
 		$strposQuestionMark = strpos( $uriRaw, '?' );
 		if ( $strposQuestionMark > 0 )
@@ -25,18 +28,43 @@ defined('_JEXEC') or die;
 			$uriClean = $uriRaw;
 		}
 		//
+		// Create the anchor tags
+		//
+		global $jmwsIdMyGadget;
+		$anchorTextPhone = "Phone View";
+		$anchorTextTablet = "Tablet View";
+		$anchorTextDesktop = "Desktop View";
+		$anchorHtmlPhone =
+			'<a href="' . $uriClean . '?gadgetType=phone" title="' . $anchorTextPhone . '">' .
+				$anchorTextPhone . '</a>';
+		$anchorHtmlTablet =
+			'<a href="' . $uriClean . '?gadgetType=tablet" title="' . $anchorTextTablet . '">' .
+				$anchorTextTablet . '</a>';
+		$anchorHtmlDesktop =
+			'<a href="' . $uriClean . '?gadgetType=desktop" title="' . $anchorTextDesktop . '">' .
+				$anchorTextDesktop . '</a>';
+		if ( $jmwsIdMyGadget->isPhone() )
+		{
+			$anchorHtmlPhone = '<span>' . $anchorTextPhone . '</span>';
+		}
+		elseif ( $jmwsIdMyGadget->isTablet() )
+		{
+			$anchorHtmlTablet = '<span>' . $anchorTextTablet . '</span>';
+		}
+		else
+		{
+			$anchorHtmlDesktop = '<span>' . $anchorTextDesktop . '</span>';
+		}
+		//
 		// Debug code:
 		// print '<p>$uriRaw: ' . $uriRaw . '</p>';
 		// print '<p>$strposQuestionMark: ' . $strposQuestionMark . '</p>';
 		// print '<p>$uriClean: ' . $uriClean . '</p>';
 	?>
 	<ul>
-		<li><a href="<?php print $uriClean . '?gadgetType=phone'; ?>" title="Emulate Phone">
-			Emulate Phone</a></li>
-		<li><a href="<?php print $uriClean . '?gadgetType=tablet'; ?>" title="Emulate Tablet">
-			Emulate Tablet</a></li>
-		<li><a href="<?php print $uriClean . '?gadgetType=desktop'; ?>" title="Emulate Desktop">
-			Emulate Desktop</a></li>
+		<li><?php print $anchorHtmlPhone; ?></li>
+		<li><?php print $anchorHtmlTablet; ?></li>
+		<li><?php print $anchorHtmlDesktop; ?></li>
 		<li><a href="<?php print $uriClean . '?gadgetType=unknown'; ?>" title="Detect Device">
 			Detect Device</a></li>
 	</ul>
