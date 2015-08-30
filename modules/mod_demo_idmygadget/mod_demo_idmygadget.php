7c70afd4ce6462c0512b4006af3b4efa0b64fe43
Tom W Hartung <tomwhartung@gmail.com>
2015-08-30 08:48:04 -0600

    Added check to ensure we have the jmwsIdMyGadget object available and it is an object of the correct type.  This should fix the errors I was seeing on the various "Demos Gone Bad."

--- modules/mod_demo_idmygadget/mod_demo_idmygadget.php
+++ modules/mod_demo_idmygadget/mod_demo_idmygadget.php
@@ -8,13 +8,20 @@
  */
 
 defined('_JEXEC') or die;
-
-if ($params->def('prepare_content', 1))
+//
+//  Don't do anything unless $jmwsIdMyGadget is set and an object of the correct class
+//
+global $jmwsIdMyGadget;
+if ( isset($jmwsIdMyGadget) )
 {
-	JPluginHelper::importPlugin('content');
-	$module->content = JHtml::_('content.prepare', $module->content, '', 'mod_demo_idmygadget.content');
+	if ( get_class($jmwsIdMyGadget) === 'JmwsIdMyGadgetJoomla' )
+	{
+		if ($params->def('prepare_content', 1))
+		{
+			JPluginHelper::importPlugin('content');
+			$module->content = JHtml::_('content.prepare', $module->content, '', 'mod_demo_idmygadget.content');
+		}
+		$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+		require JModuleHelper::getLayoutPath('mod_demo_idmygadget', $params->get('layout', 'default'));
+	}
 }
-
-$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
-
-require JModuleHelper::getLayoutPath('mod_demo_idmygadget', $params->get('layout', 'default'));
